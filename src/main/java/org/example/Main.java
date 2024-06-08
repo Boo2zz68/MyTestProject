e53ce679ac769d7d70fd9632a979f8a0a5e2e866
Пользователь <boo2zz75384@mail.ru>
2024-06-08 12:15:56 +0300

    Переход на новый тестовый сайт + работа с модальными окнами (код закомментирован)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -30,39 +30,73 @@ public class Main {
 
         //Теперь можно обращаться к драйверу
         //Например, перейти на соответствующий урл
-        driver.get("https://bananababy.ru/");
+        driver.get("https://leonardo.ru/");
         //Переменная, где храним ссылку на элемент
-        WebElement element = driver.findElement(By.xpath("//span[contains(text(),'Каталог товаров')]"));
+        WebElement element = driver.findElement(By.xpath("//a[@aria-label='Меню каталога']"));
         //Получить свойство элемента и запишем его в переменную
-        String par = element.getAttribute("height");
-        String par1 = element.getAttribute("line-height");
+        String par = element.getAttribute("hostname");
+        String par1 = element.getAttribute("contentEditable\n");
         //Выводит текст, отображаемый пользователю
         String par2 = element.getText();
         //Выведем их на экран
         System.out.println(par + " " + par1 + " " + par2);
         //Верстка стилей - обращаемся к элементу
-        //Записываем стили в переменную
+        //Записываем стили в переменную и выводим на экран
         String parametr1 = element.getCssValue("color");
         System.out.println(parametr1);
+
+        //Работа с модальными окнами (в выключенном состоянии)
+        //Есть некая кликабельная ссылка/кнопка на странице, вызывающая модальное окно
+        //WebElement element1 = driver.findElement(By.xpath("a"));
+        //element1.click();
+        //Функция alert для работы с модальными окнами
+        //Первый способ через switch, но есть вероятность, что окно не успеет подгрузится
+        //Alert alert = driver.switchTo().alert();
+        //Второй способ - через ожидания
+        //Создаем экземпляр явных ожиданий
+        //WebDriverWait wait = new WebDriverWait(driver, 1000);
+        //И запускаем alert, пока окно не появится
+        //Alert alert = wait.until(alertIsPresent());
+        //Далее прописываются действия с окном
+        //Например, если это уведомление, которое нужно принять Ок
+        //alert.accept();
+        //Если в окне есть некий ввод, то
+        //alert.sandKeys("text");
+        //alert.accept();
+        //Если в окне действия Ок и Отмена, то можно например отменить
+        //alert.dismiss();
+
         //Действие с элементом, метод клик
         //Выбираем товар, фильтрация
-        WebElement element1 = driver.findElement(By.xpath("//div[@class='index_cat_menu']//a[@class='root-item'][contains(text(),'Товары для малышей')]"));
-        WebElement element2 = driver.findElement(By.xpath("//div[@class='index_cat_menu']//a[contains(text(),'Прогулочные коляски')]"));
         //Для более сложных манипуляций с элементами новый класс action
         //На вход всегда передаем driver
         Actions action = new Actions(driver);
+        action
+                .click(element)
+                .build()
+                .perform();
+        WebElement element1 = driver.findElement(By.xpath("/html[1]/body[1]/header[1]/div[1]/div[2]/div[1]/div[1]/div[1]/nav[1]/ul[1]/li[5]/a[1]"));
+        WebElement element2 = driver.findElement(By.xpath("//a[contains(text(),'Для пэчворка')]"));
         action
                 .moveToElement(element1)
                 .click(element2)
                 .build()
                 .perform();
-        WebElement element3 = driver.findElement(By.xpath("//input[@id='arrFilter_P1_MAX']"));
-        WebElement element4 = driver.findElement(By.xpath("//input[@id='set_filter']"));
+        //Настраиваем фильтр через поле ввода
+        WebElement element3 = driver.findElement(By.xpath("//input[@placeholder='6 230']"));
+        WebElement element4 = driver.findElement(By.xpath("//a[2]//ins[1]"));
+        WebElement element5 = driver.findElement(By.xpath("//div[@class='sort-title']"));
+        WebElement element6 = driver.findElement(By.xpath("//a[contains(text(),'Цена от высокой')]"));
         action
+                .moveToElement(element3)
                 .click(element3)
                 .sendKeys(element3, Keys.DELETE)
-                .sendKeys(element3, "10000")
+                .sendKeys(element3, "5000")
+                //Вместо текста можно прописать путь до файла если импут для загрузки
+                .pause(3000)
                 .click(element4)
+                .click(element5)
+                .click(element6)
                 .release()
                 .build()
                 .perform();
