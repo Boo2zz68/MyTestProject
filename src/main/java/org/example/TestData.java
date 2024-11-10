76230049c7052530391970af235c2fb15399c3a9
Пользователь <boo2zz75384@mail.ru>
2024-11-10 12:21:04 +0300

    Создания вспомогательного класса TestData, метод генерации телефонов

---
+++ src/main/java/org/example/TestData.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Random;
+
+public class TestData {
+    public static void main(String[] args) {
+        //Вывод массива сгенерированных телефонов
+        String[] tels = new String[10];
+        for(int i=0; i< tels.length; i++) {
+            tels[i] = generateTelephone();
+            System.out.println(tels[i]);
+        }
+    }
+    //Генерация телефонов
+    public static String generateTelephone() {
+        Random random = new Random();
+        String telephone = "";
+        telephone += "+7"; //регион
+        telephone += "999"; //оператор
+        for (int i = 0; i < 7; i++) {
+            telephone += random.nextInt(10);
+        }
+        return telephone;
+    }
+}
