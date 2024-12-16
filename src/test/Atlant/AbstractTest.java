b72b9d88967a811fbb908a272011750f6545365c
Пользователь <boo2zz75384@mail.ru>
2024-12-16 16:30:07 +0300

    Создание абстрактного класса подключения к вебрайверу, общая обвязка для тестов

---
+++ src/test/Atlant/AbstractTest.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public abstract class AbstractTest {
+    private static WebDriver driver;
+    @BeforeAll
+    static void setup() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+    }
+    @BeforeEach
+    void goTo() {
+        Assertions.assertDoesNotThrow( ()-> driver.navigate().to("https://minus417ru.com/"));
+    }
+    @AfterAll
+    public static void teatDown() {
+        try {
+            Thread.sleep(4000);
+            //В конце закрываем браузер
+            driver.close();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            //Выключаем driver
+            driver.quit();
+        }
+    }
+    public static WebDriver getDriver() {
+        return driver;
+    }
+}
--- src/test/Atlant/Authorization.java
+++ src/test/Atlant/Authorization.java
@@ -1,42 +1,45 @@
+import dev.failsafe.internal.util.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
-public class Authorization {
-    @BeforeAll
-    public static void setup() {
-        WebDriverManager.chromedriver().setup();
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--incognito");
-        options.addArguments("start-maximized");
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        driver.get("https://atlant-68.ru/");
-    }
-    WebDriver driver = new ChromeDriver();
+public class Authorization extends AbstractTest {
+    //static Logger logger = LoggerFactory.getLogger(Authorization.class);
     @Test
-    void authorizationFail() {
-        driver.get("https://atlant-68.ru/");
-        WebElement personalPage = driver.findElement(By.xpath("//a[@title='Личный кабинет']//*[name()='svg']"));
-        WebElement inputLogin = driver.findElement(By.xpath("//input[@id='input-email']"));
-        WebElement buttonLogin = driver.findElement(By.xpath("//input[@value='Войти']"));
-        Actions action = new Actions(driver);
+    void switchToPersonalAccount() throws InterruptedException {
+        new WebDriverWait(getDriver(), Duration.ofSeconds(10))
+                .until(ExpectedConditions.urlContains("https://minus417ru.com/"));
+        Actions action = new Actions(getDriver());
         action
-                .moveToElement(inputLogin)
-                .click()
-                .sendKeys("Невалидный логин")
-                .moveToElement(buttonLogin)
-                .click()
+                .click(getDriver().findElement(By.xpath("//*[name()='path' and contains(@d,'M20.188 21')]")))
                 .build()
                 .perform();
-            }
+
         }
+        /*
+         action
+                .click(getDriver().findElement(By.xpath("//input[@name='PHONE_LOGIN_EMAIL']")))
+                .sendKeys("Невалидный логин")
+                .click(getDriver().findElement(By.xpath("//div[@class='bxmaker-authuserphone-enter-auth-by-password-form']//div[@class='bxmaker-authuserphone-button__inner']")))
+                .build()
+                .perform();
+        Thread.sleep(1000);
+         */
+    }
+
+
 
